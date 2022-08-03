0e0bbba3e25880aa8887e6d76036543e9fed59d2
MacGitHub2013 <bhanu.net10@gmail.com>
2022-08-03 10:32:29 +0530

    bla

---
+++ cred/views.py
@@ -0,0 +1,46 @@
+from multiprocessing import context
+from django.shortcuts import render
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.db.models.query_utils import Q
+
+
+# Create your views here.
+def password_reset_request(request):
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            associated_users = User.objects.filter(Q(email=data))
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "email.txt"
+                    c = {
+                        "email": user.email,
+                        'domain': '127.0.0.1:8000',
+                        'site_name': 'Website',
+                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                        "user": user,
+                        'token': default_token_generator.make_token(user),
+                        'protocol': 'http',
+                    }
+                    email = render_to_string(email_template_name, c)
+                    try:
+                        send_mail(subject, email, 'aryabhatta_ics@outlook.com',
+                                  [user.email], fail_silently=False)
+                    except:
+                        return HttpResponse('Invalid header found.')
+                    return redirect("/password_reset/done/")
+    password_reset_form = PasswordResetForm()
+    context = {
+        'form': password_reset_form
+    }
+    return render(request=request, template_name="password_reset_request.html", context=context)
